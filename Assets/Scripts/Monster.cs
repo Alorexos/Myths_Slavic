a4582ef41467bb942c8ae2b39a433232c7f53f8e
Wojciech Cieslinski <wojciech.cieslinski@hotmail.com>
2020-04-07 19:13:38 +0100

    Monster and Weapons
    Added basic monster model and weapon. Setup some basic damage to the monster.

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public float Health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag != "Weapon") return;
+
+        Debug.Log("Damage");
+        Health--;
+
+
+    }
+}
